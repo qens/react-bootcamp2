29b264e7647c76f8e814d2e7f1c57f29103cea91
Daniyar Bulgunayev <daniyar_bulgunayev@epam.com>
2017-04-22 14:01:57 +0600

    [Task#2] start working on tasks list

--- src/article/article.js
+++ src/article/article.js
@@ -1,32 +1,52 @@
 import React, {Component} from 'react';
+import {Checkbox, FlatButton, IconButton, Paper, TextField} from "material-ui";
+import EditorModeEdit from 'material-ui/svg-icons/editor/mode-edit';
+import './article.css';
 
-import RaisedButton from 'material-ui/RaisedButton';
+const paperStyle = {
+    width: '100%',
+    minHeight: '50px',
+    marginTop: '3px'
+};
 
 export default class Article extends Component {
 
     constructor(props) {
-        super();
+        super(props);
+    }
+
+    drawCreateTaskBlock() {
+        let textField;
+        return <div>
+            <TextField hintText="Input task" ref={(input) => textField = input}/>
+            <FlatButton onClick={() => {
+                this.props.addTask(textField.input.value);
+                textField.input.value = ''
+            }}>Add</FlatButton>
+        </div>;
+    }
+
+    drawTasksList() {
+        return <div>
+            {this.props.category.tasks.map(task =>
+                <Paper key={task.id} style={paperStyle}>
+                    <div className="task-block">
+                        <Checkbox checked={task.done}
+                                  style={{width: '20px'}}
+                                  onCheck={(event, isInputChecked) => this.props.onTaskChange(task, isInputChecked)}/>
+                        <span>{task.name}</span>
+                        <IconButton onClick={() => this.props.editTask(task)}><EditorModeEdit /></IconButton>
+                    </div>
+                </Paper>
+            )}
+        </div>;
     }
 
     render() {
-        let drawCreateTaskBlock = () => <div>
-            <input type="text" ref={(input) => this.input = input}/>
-            <RaisedButton onClick={() => this.props.addTask(this.input.value)}>Add task</RaisedButton>
-        </div>;
-
-        let drawTask = task => <li key={task.id}>
-            <input type="checkbox" checked={task.done} onChange={(event)=>this.props.onTaskChange(task, event.target.checked)}/>
-            {task.name}
-            <button onClick={()=>this.props.editTask(task)}>Edit</button>
-        </li>;
-
-        let drawTasksList = () => <div>
-            {this.props.category.tasks.map(task => drawTask(task))}
-        </div>;
 
         return (<article className="article">
-            {this.props.category && this.props.category.tasks ? drawCreateTaskBlock() : null}
-            {this.props.category && this.props.category.tasks ? drawTasksList() : null}
+            {this.props.category && this.props.category.tasks ? this.drawCreateTaskBlock() : null}
+            {this.props.category && this.props.category.tasks ? this.drawTasksList() : null}
         </article>)
     }
 
